e8b609e63743a733dd4b1a3d6485515b8716a576
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-08-05 17:11:01 -0600

    adds ability to restart tic-tac-toe game and to get some of the diagonal coordinates

--- grid-board-test/src/components/grid-board/TicTacToeBoard.jsx
+++ grid-board-test/src/components/grid-board/TicTacToeBoard.jsx
@@ -5,8 +5,10 @@ import { GridBoard, CellTemplate } from "./GridBoard";
 
 import {
   initializeGridData,
-  getAdjacentCoordinates,
   updatedBoardCell,
+  getMaxRowLineLength,
+  getMaxColLineLength,
+  getMainDiagonals,
 } from "../../utils/gridUtils";
 
 const TicTacToeBoard = () => {
@@ -16,14 +18,40 @@ const TicTacToeBoard = () => {
   const [currentTurn, setCurrentTurn] = useState("x");
   const [xWins, setXWins] = useState(0);
   const [oWins, setOWins] = useState(0);
+  const [isWinState, setIsWinState] = useState(false);
+
+  const handleInitializeGame = () => {
+    setCurrentTurn("x");
+    setGridData(initialGridData);
+    setIsWinState(false);
+    console.log("Diagonal Coordinates:");
+    console.log(getMainDiagonals(gridData));
+  };
+
+  const checkForWin = (gridData, currentTurn) => {
+    const maxLineLength = Math.max(
+      getMaxRowLineLength(gridData, currentTurn),
+      getMaxColLineLength(gridData, currentTurn)
+    );
+    if (maxLineLength === 3) {
+      console.log(`${currentTurn} wins!`);
+      setIsWinState(true);
+      currentTurn === "x"
+        ? setXWins((prev) => prev + 1)
+        : setOWins((prev) => prev + 1);
+    }
+  };
 
   const handleCellClick = (rowIndex, colIndex) => {
-    if (gridData[rowIndex][colIndex].text !== "") return;
+    if (isWinState || gridData[rowIndex][colIndex].text !== "") {
+      return;
+    }
 
     let newGridData = gridData.map((row) => row.slice());
     const newCell = new CellTemplate(currentTurn, "");
     newGridData = updatedBoardCell(newGridData, [rowIndex, colIndex], newCell);
     setGridData(newGridData);
+    checkForWin(newGridData, currentTurn);
     setCurrentTurn(currentTurn === "x" ? "o" : "x");
   };
 
@@ -33,6 +61,8 @@ const TicTacToeBoard = () => {
         <p>X Wins: {xWins}</p>
         <p>O Wins: {oWins}</p>
         <p>Current Turn: {currentTurn}</p>
+        <p>{isWinState ? "win" : "still playing"}</p>
+        <button onClick={handleInitializeGame}>Start New Game</button>
       </div>
       <GridBoard gridData={gridData} onCellClick={handleCellClick} />
     </div>
--- grid-board-test/src/utils/gridUtils.js
+++ grid-board-test/src/utils/gridUtils.js
@@ -33,3 +33,113 @@ export function getAdjacentCoordinates(
     .map(([rowShift, colShift]) => [rowIndex + rowShift, colIndex + colShift])
     .filter(([r, c]) => r >= 0 && r < rowCount && c >= 0 && c < colCount);
 }
+
+export function getMaxRowLineLength(gridData, targetText) {
+  let maxLineCount = 0;
+  gridData.forEach((row) => {
+    let rowLineCount = 0;
+    row.forEach((cell) => {
+      if (cell.text === targetText) {
+        rowLineCount += 1;
+      }
+    });
+    if (rowLineCount > maxLineCount) {
+      maxLineCount = rowLineCount;
+    }
+  });
+  return maxLineCount;
+}
+
+export function getMaxColLineLength(gridData, targetText) {
+  let maxLineCount = 0;
+  for (let i = 0; i < gridData[0].length; i++) {
+    let colLineCount = 0;
+    gridData.forEach((row) => {
+      if (row[i].text === targetText) {
+        colLineCount += 1;
+      }
+    });
+    if (colLineCount > maxLineCount) {
+      maxLineCount = colLineCount;
+    }
+  }
+  return maxLineCount;
+}
+
+export function getMainDiagonals(gridData) {
+  const rowCount = gridData.length;
+  const colCount = gridData[0].length;
+  const mainDiagonalCoordinates = [];
+  // 0,0 and down (upper left to bottom right)
+
+  for (let i = 0; i < rowCount; i++) {
+    const lineCoordinates = [];
+    let rowIndex = i;
+    let colIndex = 0;
+    while (rowIndex <= rowCount - 1 && colIndex <= colCount - 1) {
+      lineCoordinates.push([rowIndex, colIndex]);
+      rowIndex++;
+      colIndex++;
+    }
+    mainDiagonalCoordinates.push(lineCoordinates);
+  }
+
+  for (let i = 1; i < colCount; i++) {
+    const lineCoordinates = [];
+    let rowIndex = 0;
+    let colIndex = i;
+
+    while (rowIndex <= rowCount - 1 && colIndex <= colCount - 1) {
+      console.log("Coordinates", rowIndex, colIndex);
+      lineCoordinates.push([rowIndex, colIndex]);
+      rowIndex++;
+      colIndex++;
+    }
+    mainDiagonalCoordinates.push(lineCoordinates);
+  }
+
+  return mainDiagonalCoordinates;
+}
+
+export function getReverseDiagonals(gridData) {
+  const rowCount = gridData.length;
+  const colCount = gridData[0].length;
+  const reverseDiagonalsCoordinates = [];
+  // 0,0 and down (upper left to bottom right)
+
+  // for (let i = 0; i < rowCount; i++) {
+  //   const lineCoordinates = [];
+  //   let rowIndex = i;
+  //   let colIndex = 0;
+  //   while (rowIndex <= rowCount - 1 && colIndex <= colCount - 1) {
+  //     lineCoordinates.push([rowIndex, colIndex]);
+  //     rowIndex++;
+  //     colIndex++;
+  //   }
+  //   reverseDiagonalsCoordinates.push(lineCoordinates);
+  // }
+
+  // for (let i = 1; i < colCount; i++) {
+  //   const lineCoordinates = [];
+  //   let rowIndex = 0;
+  //   let colIndex = i;
+
+  //   while (rowIndex <= rowCount - 1 && colIndex <= colCount - 1) {
+  //     console.log("Coordinates", rowIndex, colIndex);
+  //     lineCoordinates.push([rowIndex, colIndex]);
+  //     rowIndex++;
+  //     colIndex++;
+  //   }
+  //   reverseDiagonalsCoordinates.push(lineCoordinates);
+  // }
+
+  // return reverseDiagonalsCoordinates;
+}
+
+// export function getMaxDiagonalLineLength(gridData, targetText) {
+//   let maxLineCount = 0;
+
+//   for (let i = 0; i < gridData.length; i++) {
+//     let lineCount = 0;
+//   }
+// }
