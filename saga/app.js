4064b163e3fc57e60bd8534f5f7e34a73a67d2c3
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-08-07 14:11:19 -0600

    adds basic setup for looking at saga stats

---
+++ saga/app.js
@@ -0,0 +1,42 @@
+const statGrowthData = [
+  {
+    name: "Annie",
+    growthStats: [28, 5, 3, 1, 4, 1, 3, 5, 6],
+  },
+  {
+    name: "Asellus",
+    growthStats: [30, 0, 2, 1, 6, 9, 0, 9, 3],
+  },
+  {
+    name: "",
+    growthStats: [33 | 0 | 5 | 8 | 8 | 6 | 1 | 2 | 3],
+  },
+  {
+    name: "",
+    growthStats: [],
+  },
+  {
+    name: "",
+    growthStats: [],
+  },
+  {
+    name: "",
+    growthStats: [],
+  },
+  {
+    name: "",
+    growthStats: [],
+  },
+  {
+    name: "",
+    growthStats: [],
+  },
+  {
+    name: "",
+    growthStats: [],
+  },
+  {
+    name: "",
+    growthStats: [],
+  },
+];
