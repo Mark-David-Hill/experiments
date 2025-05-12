32270756781bc2155a493675f4aab00de96ecb07
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-05-11 21:34:18 -0600

    sets up functions for different activities to choose and for time to pass

--- marshmallow/src/App.jsx
+++ marshmallow/src/App.jsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [happiness, setHappiness] = useState(10);
   const [hunger, setHunger] = useState(0);
   const [stress, setStress] = useState(0);
   const [money, setMoney] = useState(20);
   const [food, setFood] = useState(0);
+  const [strength, setStrength] = useState(5);
+  const [speed, setSpeed] = useState(5);
+  const [stamina, setStamina] = useState(5);
+  const [intelligence, setIntelligence] = useState(5);
   const [skill, setSkill] = useState(10);
   const [week, setWeek] = useState(1);
 
@@ -20,10 +21,30 @@ function App() {
     }
   };
 
+  const play = () => {
+    setStress((prevStress) => prevStress - 10);
+    setHappiness((prevHappiness) => prevHappiness + 5);
+    weekPasses();
+  };
+
+  const train = () => {
+    setSkill((prev) => prev + 10);
+    setStress((prev) => prev + 5);
+    weekPasses();
+  };
+
+  const work = () => {
+    setMoney((prev) => prev + 50);
+    setHappiness((prev) => prev - 3);
+    setStress((prev) => stress + 3);
+    weekPasses();
+  };
+
   const feed = () => {
     if (food > 0) {
       setFood((prev) => prev - 1);
       setHunger((prev) => (prev - 10 > 0 ? prev - 10 : 0));
+      setHappiness((prev) => prev + 1);
     }
   };
 
@@ -44,13 +65,13 @@ function App() {
       <h2>Week: {week}</h2>
       <p>What do you want to do this week?</p>
       <div className="week-choice">
-        <button onClick={weekPasses}>Do nothing</button>
-        <button>Play with Marhsmallow</button>
-        <button>Train Marshmallow</button>
-        <button>Leave Marshmallow for Work</button>
+        <button onClick={play}>Play with Marhsmallow</button>
+        <button onClick={train}>Train Marshmallow</button>
+        <button onClick={work}>Leave Marshmallow for Work</button>
       </div>
       <div className="card">
         <div className="stats">
+          <p>Happiness: {happiness}</p>
           <p>Hunger: {hunger}</p>
           <p>Stress: {stress}</p>
           <p>Money: {money}</p>
@@ -64,9 +85,6 @@ function App() {
         <button onClick={buyFood}>Buy Food ($10)</button>
         <button onClick={feed}>Feed Marshmallow</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
